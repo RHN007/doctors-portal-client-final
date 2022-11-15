d75aaf23e6991c8e499932048eef76fdb4105171
RHN007 <rakibul.nayon@outlook.com>
2022-11-15 06:53:49 +0600

    adding service

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Banner from '../../Shared/Banner/Banner';
+import CardInfo from '../Card/CardInfo';
+
+
 
 const Home = () => {
     return (
         <div className='mx-5'>
             <h1>this is login </h1>
+            <Banner></Banner>
+            <CardInfo></CardInfo>
+            
         </div>
     );
 };
--- src/pages/Shared/Banner/Banner.js
+++ src/pages/Shared/Banner/Banner.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import chair from '../../assets/images/chair.png';
-import PrimaryButton from '../Shared/PrimaryButton';
+import chair from '../../../assets/images/chair.png';
+// import PrimaryButton from '../../../assets/';
 
 const Banner = () => {
     return (
-        <div className="hero min-h-screen bg-[url('/src/assets/images/bg.png')] mb-1">
+        <div className="hero  bg-[url('/src/assets/images/bg.png')] mb-1">
             <div className="hero-content flex-col lg:flex-row-reverse">
-                <img src={chair} className="max-w-sm rounded-lg shadow-2xl" alt='chair' />
+                <img src={chair} className="lg:w-1/2 rounded-lg shadow-2xl" alt='chair' />
                 <div>
                     <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
                     <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                   <PrimaryButton>Get Started</PrimaryButton>
+                   <button className=" btn btn-primary bg-gradient-to-r from-primary to-secondary">Get Started</button>
                 </div>
             </div>
         </div>
