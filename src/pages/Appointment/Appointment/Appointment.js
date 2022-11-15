ccf886f78c1f37f85781a802b3ac30e83fbe5a90
RHN007 <rakibul.nayon@outlook.com>
2022-11-15 13:29:18 +0600

    adding title and appointemt router and appointment banner

---
+++ src/pages/Appointment/Appointment/Appointment.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AppointmentBanner from '../AppointmentBanner/AppointmentBanner';
+
+const Appointment = () => {
+    return (
+        <div>
+            <h1>This is an appointment </h1>
+            <AppointmentBanner></AppointmentBanner>
+        </div>
+    );
+};
+
+export default Appointment;
---
+++ src/pages/Appointment/AppointmentBanner/AppointmentBanner.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import chair from '../../../assets/images/chair.png'
+import { DayPicker } from 'react-day-picker';
+import { format } from 'date-fns';
+import useTitle from '../../../hooks/UseTitle';
+
+const AppointmentBanner = () => {
+    useTitle('Appointment')
+    const [selectedDate, setSelectedDate] = useState(new Date())
+
+    return (
+        <header className='my-6'>
+            <div className="hero ">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <img src={chair} className="max-w-sm rounded-lg shadow-2xl" alt='Dentist chair' />
+                    <div className='mr-6'>
+                        <DayPicker
+                        mode="single"
+                        selected={selectedDate}
+                        onSelect={setSelectedDate}
+                        showOutsideDays 
+                        
+                        />
+                        <p>You have Selected Date: {format(selectedDate, 'PPPP')}</p>
+                       
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default AppointmentBanner;
